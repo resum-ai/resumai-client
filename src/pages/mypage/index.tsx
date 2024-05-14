ef5c9453050ab391df4a449a85383f00aaff53de
kangnayeon <seoultownroad@naver.com>
2024-05-14 09:13:53 +0900

    feat: UserInfoArea 추가

--- src/pages/mypage/index.tsx
+++ src/pages/mypage/index.tsx
@@ -1,3 +1,28 @@
+import { Flex, Text } from '@/components/Wrapper';
+import styled from '@emotion/styled';
+
 export const MyPage = () => {
-  return <div>mypage</div>;
+  return (
+    <div>
+      <UserInfoArea direction="column" gap={12}>
+        <Text typo="title" color="black">
+          닉네임
+        </Text>
+        <Flex>
+          <Text typo="main_text" color="black">
+            작성 중인 자소서:
+          </Text>
+          <Text typo="main_text" color="blue_100">
+            &nbsp;5개
+          </Text>
+        </Flex>
+      </UserInfoArea>
+    </div>
+  );
 };
+
+const UserInfoArea = styled(Flex)`
+  width: 100vw;
+  height: 180px;
+  border-bottom: 1px solid ${({ theme }) => theme.palette.gray2};
+`;
