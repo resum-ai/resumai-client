3a83e5e85af645b4c2a29480c5d1139961cdd045
kangnayeon <seoultownroad@naver.com>
2024-05-16 05:41:36 +0900

    feat: 채팅 api 연결

--- src/pages/create/Chat.tsx
+++ src/pages/create/Chat.tsx
@@ -1,3 +1,6 @@
+/* eslint-disable no-sparse-arrays */
+/* eslint-disable no-unsafe-optional-chaining */
+/* eslint-disable @typescript-eslint/no-non-null-asserted-optional-chain */
 import { resumeApi } from '@/apis/resume';
 import { Flex } from '@/components/Wrapper';
 import { ResumeTitle } from '@/components/create/ResumeTitle';
@@ -5,27 +8,57 @@ import { ChatArea } from '@/components/create/chat/ChatArea';
 import { ChatBalloonProps } from '@/components/create/chat/ChatBalloon';
 import { ChatInput } from '@/components/create/chat/ChatInput';
 import styled from '@emotion/styled';
-import { useQuery } from '@tanstack/react-query';
-import { useState } from 'react';
+import { useMutation, useQuery } from '@tanstack/react-query';
+import { useEffect, useState } from 'react';
 import { useFormContext } from 'react-hook-form';
 import { useParams } from 'react-router-dom';
 
 export const Chat = () => {
-  const { watch } = useFormContext();
+  const { watch, setValue } = useFormContext();
   const { id } = useParams();
-  const { data, isFetching } = useQuery({
+  const { data } = useQuery({
     queryKey: ['chat', id],
     queryFn: () => resumeApi.GET_RESUME_CHAT_HISTORY(Number(id))
   });
 
-  console.log(data, isFetching);
+  const [chats, setChats] = useState<ChatBalloonProps[]>([]);
 
-  const [chats, setChats] = useState<ChatBalloonProps[]>([
-    {
-      isUser: false,
-      text: '다음은 AI가 작성한 자기소개서입니다. 수정을 원하시는 부분이 있으신가요?'
+  useEffect(() => {
+    if (data?.results) {
+      setChats([
+        {
+          is_user: false,
+          content:
+            '다음은 AI가 작성한 자기소개서입니다. 수정을 원하시는 부분이 있으신가요?'
+        },
+        ...data.results
+      ]);
     }
-  ]);
+  }, [data]);
+
+  const sendChatMutation = useMutation({
+    mutationFn: ({ id, query }: { id: number; query: string }) =>
+      resumeApi.POST_RESUME_CHAT({ id: id, query: query }),
+    onMutate: () => {
+      setValue('chat', 'AI가 답변을 생성하는 중입니다.');
+    },
+    onSuccess: ({ answer }: { answer: string }) => {
+      setChats((prev) => {
+        setValue('chat', '');
+        return [...prev, { is_user: false, content: answer }];
+      });
+    },
+    onError: () => {}
+  });
+
+  const handleSendClick = () => {
+    setChats((prev) => {
+      return [...prev, { is_user: true, content: watch('chat') }];
+    });
+    sendChatMutation.mutate({ id: Number(id), query: watch('chat') });
+  };
+
+  // TODO onSubmit 부분 작성하고 뮤테이션 걸기
 
   const onSubmit = () => {};
 
@@ -35,9 +68,10 @@ export const Chat = () => {
         title={watch('title')}
         onSubmit={onSubmit}
         submitButtonText="완성하기"
+        disabled={!watch('checkedChat')}
       />
-      <ChatArea chats={chats} />
-      <ChatInput chats={chats} setChats={setChats} />
+      {chats && <ChatArea chats={chats} />}
+      <ChatInput handleSendClick={handleSendClick} />
     </Wrapper>
   );
 };
